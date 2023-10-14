caf2f3df4c12e705d72595ec8963ad8a8189601b
Alex Farup Christensen <56341143+AlexFarup@users.noreply.github.com>
2023-10-14 14:45:36 +0200

    Add bestyrelsen video

--- bestyrelsen/settings.py
+++ bestyrelsen/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'bestyrelsen',
         'USER': 'postgres',
-        'PASSWORD': '1234',
+        'PASSWORD': 'vxyz1234',
         'PORT': '5432'
     }
 }
